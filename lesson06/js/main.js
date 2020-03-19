6937c416565939b9ce90a76b851fe9c97ebddbf8
Santiago Medina <carupanodev@gmail.com>
2020-03-19 01:42:31 -0400

    Vue js practice

---
+++ lesson06/js/main.js
@@ -0,0 +1,47 @@
+var vm = new Vue({
+		el : '#app',
+		data : {
+			tasks:[
+				{
+					title: 'Learn VueJs.',
+					pending: false
+				},
+				{
+					title: 'Get a job at Mnonline',
+					pending: true
+				},
+				{
+					title: 'Bring joy to my family',
+					pending: true
+				}
+			],
+			newTask : '',
+		},
+		computed:{
+			pendingTasks : function (){
+				return this.tasks.filter(function(item){
+					return item.pending;
+				});
+			}
+		},
+		methods:{
+			addTask(pending, event){
+				event.preventDefault();
+				if(this.newTask != ''){
+					this.tasks.push({title: this.newTask, pending: pending});	
+				}
+				else{
+					console.log('d');
+				}
+
+				this.newTask = '';
+			},
+			changeTaskStatus(task){
+				task.pending ? task.pending = false : task.pending = true
+			},
+
+			taskStyle(task){
+				return task.pending ? 'text-danger' : 'text-del';
+			}
+		}
+	});
